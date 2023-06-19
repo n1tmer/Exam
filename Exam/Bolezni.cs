8f34b23fceea18f902eec5658296e8699edb81bd
is13131 <is13131@B206-10>
2023-06-19 14:01:11 +0700

    Добавьте файлы проекта.

---
+++ Exam/Bolezni.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Bolezni
+    {
+        public string bolezn { get; set; }
+        public Bolezni(string _bolezn)
+        {
+            bolezn = _bolezn;
+        }
+    }
+  
+}
---
+++ Exam/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam
+{
+    public partial class Form1 : Form
+    {
+        private List<Bolezni> Заболевания { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            Заболевания = new List<Bolezni>();
+            Заболевания.Add(new Bolezni("Gripp"));
+            Заболевания.Add(new Bolezni("Рак"));
+            Заболевания.Add(new Bolezni("Ветрянка"));
+            Заболевания.Add(new Bolezni("Желтуха"));
+            Заболевания.Add(new Bolezni("Гастрит"));
+            Заболевания.Add(new Bolezni("Язва"));
+            dataGridView1.DataSource = Заболевания.ToList();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Заболевания.Where(u => u.bolezn == textBox1.Text).ToList();
+            if (textBox1.Text == "")
+            {
+                dataGridView1.DataSource = Заболевания.ToList();
+            }
+        }
+    }
+}
